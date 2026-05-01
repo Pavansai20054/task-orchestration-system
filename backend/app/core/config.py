ee06e6eec3af2e3d22855df7c8c297691f01bf66
Pavansai Rangdal <pavansai87654321@gmail.com>
2026-05-01 20:06:14 +0000

    fix: .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    CORS_ORIGINS: str = "http://localhost:5173,http://127.0.0.1:5173"
+    CORS_ORIGINS: str
 
     class Config:
         env_file = ".env"
